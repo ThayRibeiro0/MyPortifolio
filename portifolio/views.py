507ea3c7bd37eaacaaceda24ce77625d07d08ec9
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-10-23 08:23:02 -0700

    Fist commit

---
+++ portifolio/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route('/about')
+def about():
+    return render_template("about.html")
+
+@views.route('/experiences')
+def experiences():
+    return render_template("experiences.html")
+
+@views.route('/skills')
+def skills():
+    return render_template("skills.html")
+
+@views.route('/contact')
+def contact():
+    return render_template("contact.html")
