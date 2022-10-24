9e0109c09ae8b228ae7b88e2d62124581d7f2298
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-10-24 12:33:28 -0700

    5 commit

--- portifolio/static/index.js
+++ portifolio/static/index.js
@@ -3,3 +3,10 @@ alert('Hello World');
 // function togglenav() {
 //     $('nav').toggleClass('open')
 // }
+
+var typed = new Typed(".auto-type", {
+    strings: ["Lawyer", "Systems Developer", "Administrator", "Lawyer"],
+    typeSpeed: 130,
+    backSpeed: 100,
+    loop: true
+});
